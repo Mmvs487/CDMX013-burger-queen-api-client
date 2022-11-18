668774284231bd4dff5323ae6e58ae9d6f7b4f17
SaumarDev <saumar_1@hotmail.com>
2022-11-17 19:25:05 -0600

    feat: styles of login component

--- el-carinito/src/components/no-auth/Login.js
+++ el-carinito/src/components/no-auth/Login.js
@@ -1,19 +1,28 @@
-import logo from '../../images/logo-carinito.png'
-import './login.css'
+import logo from "../../images/logo-carinito.png";
+import "./login.css";
 
-export default function Login(){
-return(
-    <div className='container'>
-        <div className='logoContainer'>
-            <img src={logo} alt="logo"></img>
+export default function Login() {
+  return (
+    <div className="container">
+      <img src={logo} alt="logo"></img>
+
+      <div className="inputsContainer">
+        <div className="emailContainer">
+          <input
+            type="text"
+            name="email"
+            placeholder="E-MAIL"
+            className="inputLogin"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="PASSWORD"
+            className="inputLogin"
+          />
+          <button className="login-button">LOG IN</button>
         </div>
-        <div className='inputsContainer'>
-           <div className='emailContainer'>
-            <input type='text' name='email' placeholder='E-MAIL' className='inputLogin'/>
-            <input type='password' name='password' placeholder='PASSWORD' className='inputLogin'/>
-            <button className='login-button'>LOG IN</button>
-            </div>
-        </div>
+      </div>
     </div>
-)
+  );
 }
