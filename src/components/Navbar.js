96b3c9efad58d2e86ccc10251de199b6bdb89b06
Mmvs487 <monicavela@ciencias.unam.mx>
2022-12-06 19:53:01 -0600

    Dynamic props passed to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,10 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import "./navbar.css"
-import createOrderTitle from "../images/create-order-text.png"
 import logoutButton from "../images/logout.png"
-import newOrderIcon from "../images/new-order-icon.png"
-import completedOrdersIcon from "../images/completed-orders-icon.png"
 
-export function Navbar({ handleSaveUser }) {
+export function Navbar({ handleSaveUser, children }) {
     const navigate = useNavigate();
-    /* const newOrders = () =>{
-        navigate('/orders');
-      } */
-    const completedOrders = () =>{
-        navigate('/completed-orders');
-    }
 
     const logOut = () => {
         handleSaveUser(null)
@@ -22,20 +13,15 @@ export function Navbar({ handleSaveUser }) {
     }
     
     return (
-    <nav className="nav">
+    <nav>
+        { handleSaveUser &&
+        <div className="navbar">
         <div className="defaults">
         <img className="log-out" src={logoutButton} alt="logout" onClick={logOut}/>
-        <img className="title" src={createOrderTitle} alt='page-title'/>
         </div>
-        <ul>
-        <p className="user-email">{localStorage.getItem("email")}</p>
-            <li>
-            <img className="dinamicIcons" src={newOrderIcon} alt='create-order'/>
-            </li>
-            <li>
-            <img className="dinamicIcons" src={completedOrdersIcon} alt='completed-orders' onClick={completedOrders}/>
-            </li>
-        </ul>
+        {children}
+        </div>
+        }
     </nav>
     )
 }
